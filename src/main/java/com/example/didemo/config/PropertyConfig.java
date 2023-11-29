f8fcd70a7dca870b4d06ce2ba3bb5ed45e8e56cd
hagar <hagarnabil7@gmail.com>
2023-11-29 16:51:32 +0200

    adding env var

--- src/main/java/com/example/didemo/config/PropertyConfig.java
+++ src/main/java/com/example/didemo/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.example.didemo.config;
 
 import com.example.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+    @Autowired
+    Environment env;
+
     @Value("${guru.username}")
     String user;
     @Value("${guru.password}")
@@ -19,7 +24,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("GURU_USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
