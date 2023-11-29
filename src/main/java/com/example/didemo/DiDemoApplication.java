237165cafc8ea2e1cc302df467ddf8724776b343
hagar <hagarnabil7@gmail.com>
2023-11-29 16:46:01 +0200

    adding fake data source

--- src/main/java/com/example/didemo/DiDemoApplication.java
+++ src/main/java/com/example/didemo/DiDemoApplication.java
@@ -4,13 +4,14 @@ import com.example.didemo.controllers.ConstructorInjectedController;
 import com.example.didemo.controllers.MyController;
 import com.example.didemo.controllers.PropertyInjectedController;
 import com.example.didemo.controllers.SetterInjectedController;
+import com.example.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example","services"})
+@ComponentScan(basePackages = {"com.example"})
 public class DiDemoApplication {
 
     public static void main(String[] args) {
@@ -19,10 +20,14 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
+
+//        System.out.println(controller.hello());
+//        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+//        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+//        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
     }
 
 }
