5807b2a512f68d89c579f6d4a4dcdabf4e182cb5
hagar <hagarnabil7@gmail.com>
2023-11-29 17:01:56 +0200

    many profiles

--- src/main/java/com/example/didemo/config/PropertyConfig.java
+++ src/main/java/com/example/didemo/config/PropertyConfig.java
@@ -1,20 +1,15 @@
 package com.example.didemo.config;
 
 import com.example.didemo.examplebeans.FakeDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
-    @Autowired
-    Environment env;
-
     @Value("${guru.username}")
     String user;
     @Value("${guru.password}")
@@ -24,7 +19,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("GURU_USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
